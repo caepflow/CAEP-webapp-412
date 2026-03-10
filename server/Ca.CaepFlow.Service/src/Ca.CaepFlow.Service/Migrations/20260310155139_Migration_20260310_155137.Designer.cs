5ead51b435eb56b5e301d8820f926baca5f9afb5
streampulse <streampulse@noreply.com>
2026-03-10 15:51:40 +0000

    chore: Add database migrations for project fba0c046-57be-4f5a-a809-e358c721f234

--- server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260310155139_Migration_20260310_155137.Designer.cs
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260310155139_Migration_20260310_155137.Designer.cs
@@ -4,6 +4,7 @@ using Ca.CaepFlow.Service.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ca.CaepFlow.Service.Migrations
 {
     [DbContext(typeof(NewServiceDataContext))]
-    partial class NewServiceDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260310155139_Migration_20260310_155137")]
+    partial class Migration_20260310_155137
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260310155139_Migration_20260310_155137.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20260310_155137 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Product");
+        }
+    }
+}
